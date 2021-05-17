c1fa08e9b324dd5de87d82d2e1bb5626618b1ba2
Edgar Almeida <edgardc.almeida@outlook.com>
2021-05-17 13:55:54 -0300

    test(categories and genres): add tests

---
+++ tests/Unit/Models/GenreTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Genre;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid;
+
+class GenreTest extends TestCase
+{
+    public function testFillableAttribute()
+    {
+        $fillables = ['name', 'is_active'];
+        $genre = new Genre();
+        $this->assertEquals($fillables, $genre->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [SoftDeletes::class, Uuid::class];
+        $genreTraits = array_keys(class_uses(Genre::class));
+        $this->assertEquals($traits, $genreTraits);
+    }
+
+    public function testCastsAttribute()
+    {
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $genre = new Genre();
+        $this->assertEquals($casts, $genre->getCasts());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $genre = new Genre();
+
+        $this->assertEqualsCanonicalizing($dates, $genre->getDates());
+    }
+}
