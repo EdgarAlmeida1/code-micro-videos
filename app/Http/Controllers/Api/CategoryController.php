d7103190fe925b023313b1ab3922f82e8174597f
Edgar Almeida <edgardc.almeida@outlook.com>
2021-05-17 11:06:32 -0300

    feat(category): add model category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private $rules = [
+        "name" => "required|max:255",
+        "description" => "max:255",
+        "is_active" => "boolean"
+    ];
+
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate($rules);
+        $category = Category::create($request->all());
+        $category->refresh();
+        return $category;
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate($this->rules);
+        $category->update($request->all());
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes, Traits\Uuid;
+
+    protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
+    ];
+    public $incrementing = false;
+    protected $keyType = 'string';
+}
