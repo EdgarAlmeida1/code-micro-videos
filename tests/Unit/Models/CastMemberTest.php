0b4d27c0083cdf37d3c1a83190440d4fec6fb490
Edgar Almeida <edgardc.almeida@outlook.com>
2021-05-20 10:55:37 -0300

    feat(cast members): add model cast members

---
+++ tests/Unit/Models/CastMemberTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\CastMember;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid;
+
+class CastMemberTest extends TestCase
+{
+    public function testFillableAttribute()
+    {
+        $fillables = ['name', 'type'];
+        $castMember = new CastMember();
+        $this->assertEquals($fillables, $castMember->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [SoftDeletes::class, Uuid::class];
+        $castMemberTraits = array_keys(class_uses(CastMember::class));
+        $this->assertEquals($traits, $castMemberTraits);
+    }
+
+    public function testCastsAttribute()
+    {
+        $casts = ['id' => 'string'];
+        $castMember = new CastMember();
+        $this->assertEquals($casts, $castMember->getCasts());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $castMember = new CastMember();
+
+        $this->assertEqualsCanonicalizing($dates, $castMember->getDates());
+    }
+}
